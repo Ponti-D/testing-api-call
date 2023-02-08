8bb0c445872727579eed376d84962fed33ef68cb
Ponti-D <spaediaz@gmail.com>
2023-02-08 08:02:12 +0000

    Implemeted App.tsx to fetch data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [people, setPeople] = useState("");
+
+  useEffect(() => {
+    getSWAPIPeople();
+  }, []);
+
+  const getSWAPIPeople = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/people/1/");
+      if (response.status === 500) {
+        setPeople("Oops… something went wrong, try again");
+      }
+      const result = await response.json();
+
+      setPeople(result.name);
+    } catch (error) {
+      setPeople("Oops… something went wrong, try again");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>SWAPI People</h1>
+        <p>{people}</p>
       </header>
     </div>
   );

9ab44edf52808a2c9f99537f2a9d60adb6aaed73
Ponti-D <spaediaz@gmail.com>
2023-02-08 08:16:27 +0000

    Implemeted test using msw for mocking API call

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 import App from './App';
 
-test('renders learn react link', () => {
+const server = setupServer(
+  rest.get('https://swapi.dev/api/people/1/', (req, res, ctx) => {
+    return res(ctx.json({name: 'Luke Skywalker'}));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+
+test('Check if first person is Luke Skywalker', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  let name =await screen.findByText('Luke Skywalker')
+  expect(name).toBeInTheDocument();
 });
+
